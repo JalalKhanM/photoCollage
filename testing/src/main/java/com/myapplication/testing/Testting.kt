446e67c1ab046c053d4583d1a962e99ffe2d258c
JalalKhanM <jalalkhan.mobipixels@gmail.com>
2023-02-01 11:53:40 +0500

    commit2

--- testing/src/main/java/com/myapplication/testing/Testting.kt
+++ testing/src/main/java/com/myapplication/testing/Testting.kt
@@ -1,17 +1,27 @@
 package com.myapplication.testing
 
+
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.collage_special.MainActivity
+
+import androidx.appcompat.app.AppCompatActivity
 import com.example.collage_special.SelectImageActivity
 
+
 class Testting : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_testting)
 
+
         val intent = Intent(this, SelectImageActivity::class.java)
         startActivity(intent)
+
+
     }
+
+
+
 }
